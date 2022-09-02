9a3bcc722ed0ba800de6a69b6be497d2600dd08a
mattrichor <matt.a.geyer@gmail.com>
2022-09-02 13:53:35 -0400

    set up basis for models / relationships. Seeded dummy models

--- wabi_django/wabi/admin.py
+++ wabi_django/wabi/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Sketch, Prompt
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Sketch)
+admin.site.register(Prompt)
--- wabi_django/wabi/models.py
+++ wabi_django/wabi/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(
+        verbose_name='email address', max_length=255, unique=True)
+    username = models.CharField(max_length=100, null=True)
+    password = models.CharField(max_length=100,)
+    is_active = models.BooleanField(default=False)
+    friends = models.ManyToManyField(
+        'self', related_name='friends', blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Prompt(models.Model):
+    date = models.DateField()
+    text = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.text
+
+
+class Sketch(models.Model):
+    sketch_data = models.JSONField()
+    date = models.DateField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user', null=True)
+    prompt = models.ForeignKey(
+        Prompt, on_delete=models.CASCADE, related_name='prompt', null=True)
